67ee27fe23a0621fb2bed5c80a0cc52f1df11a7b
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-09-08 19:29:55 +0300

    created a submission

--- script.js
+++ script.js
@@ -34,13 +34,21 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    if (lookup[char] !== undefined) {
+      decodedArr.push(lookup[char]);
+    } else {
+      decodedArr.push(char);
+    }
+  }
 
   return; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+ console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
